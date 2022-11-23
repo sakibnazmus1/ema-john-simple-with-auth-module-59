c6f8d532192a22f176ba1e37cd86fed43547813f
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-23 15:58:37 +0600

    login page added

---
+++ src/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+
+
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDWVSTrA-UoVOkBwp53u6ssMv6_Rzg1MD0",
+    authDomain: "ema-john-simple-a75cb.firebaseapp.com",
+    projectId: "ema-john-simple-a75cb",
+    storageBucket: "ema-john-simple-a75cb.appspot.com",
+    messagingSenderId: "30961008025",
+    appId: "1:30961008025:web:557de429b382b316e46708"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
