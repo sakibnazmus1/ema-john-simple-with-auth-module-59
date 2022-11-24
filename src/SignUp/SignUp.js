de059530c69815a0f5d72197e2aed42d6754b199
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-24 18:40:55 +0600

    sign up added

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/UserContext';
 import './SignUp.css';
 
 const SignUp = () => {
-
+    const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext);
     const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
         const confirm = form.confirm.value;
-        console.log(email, password, confirm);
+
+        if (password.length < 6) {
+            setError('Password should be 6 charecters or more');
+            return;
+        }
+        if (password !== confirm) {
+            setError('Your Password did not match');
+            return;
+        }
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(result => {
+                console.error(error);
+            })
+
     }
 
     return (
@@ -32,6 +54,7 @@ const SignUp = () => {
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p>Already Have an Account? <Link to='/login'>Login</Link></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,11 +1,19 @@
 import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import app from '../firebase/firebase.config';
+import { useState } from 'react';
 
 export const AuthContext = createContext();
+const auth = getAuth(app)
 
 const UserContext = ({ children }) => {
+    const [user, setUser] = useState(null);
 
-    const user = { email: 'Password' }
-    const authInfo = { user }
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const authInfo = { user, createUser }
 
 
     return (
