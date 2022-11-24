63c2436408c8ea5711ad2406f9c2358aa5711499
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-24 16:25:38 +0600

    update Context Api

--- src/__auth.step.js
+++ src/__auth.step.js
@@ -4,4 +4,9 @@
 // 3. enable sign in method
 // 4. install firebase
 // 5. get firebase config in your firebase.config.js file
-// 6. export app from firebase.config.js file 
+// 6. export app from firebase.config.js file
+
+
+// 1. Create UserContext (Auth Context): UserContext--->component name, userContext provides AuthContext
+
+// 2. Create AuthContext
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const About = () => {
+
     return (
         <div>
             <h2>Secret about us!!!</h2>
