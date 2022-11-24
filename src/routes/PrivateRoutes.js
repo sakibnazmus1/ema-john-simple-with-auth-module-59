9ad8d5d34e5b0be7c7d4c9cf63dd93d170792e41
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-24 23:00:26 +0600

    navigation added

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/UserContext';
 
 const PrivateRoutes = ({ children }) => {
     const { user } = useContext(AuthContext);
+    const location = useLocation();
     if (user && user.id) {
         return children;
     }
-    return <Navigate to='/login'></Navigate>
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
 };
 
 export default PrivateRoutes;
