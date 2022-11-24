f52ca0f6ed9db4ef1ee4956429cb7b0e70b97fd4
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-24 17:19:38 +0600

    handleSubmit added

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -3,10 +3,20 @@ import { Link } from 'react-router-dom';
 import './SignUp.css';
 
 const SignUp = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" required />
