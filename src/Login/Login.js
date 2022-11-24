98cba4d261e5184e887403b15acc6b0739b03749
sakibnazmus1 <sakibnazmus438@gmail.com>
2022-11-24 10:51:28 +0600

    signup added

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Login" />
             </form>
-            <p>New to Ema-john <Link to='/signup'>Create a New Account</Link></p>
+            <p>New to Ema-john? <Link to='/signup'>Create a New Account</Link></p>
         </div>
     );
 };
